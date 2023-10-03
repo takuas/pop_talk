6a4cf9a473f5cb61577468000dd8fb11419fa468
takuas <365.taku.as@gmail.com>
2023-10-03 21:24:02 +0900

    userモデルのバリデーション再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates name:, presence: true
-  validates email, presence: true
-  validates password, presence: true
+  validates :name, presence: true
 end
